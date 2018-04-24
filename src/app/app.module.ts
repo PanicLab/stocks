9d051edc2228bf65739da3362d8fc22e63bee478
PanicLab <PanicLabLimited@gmail.com>
2018-04-24 15:21:12 +0400

    2.6 finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule  } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { StocksService } from './services/stocks.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    StocksService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
